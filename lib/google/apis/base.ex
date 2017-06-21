b798688334ec09218f82179ffbc94130374e7e80
elkelk <danielshaneupton@gmail.com>
2017-06-21 10:07:13 -0400

    Ignore key for Oauth methods

--- lib/google/apis/base.ex
+++ lib/google/apis/base.ex
@@ -1,6 +1,6 @@
 defmodule Google.Apis.Base do
   defmacro __using__(opts) do
-    quote bind_quoted: [endpoint: opts[:endpoint]] do
+    quote bind_quoted: [endpoint: opts[:endpoint], oauth_only: opts[:oauth_only]] do
       use HTTPoison.Base
 
       defp process_response_body(body) do
@@ -9,8 +9,18 @@ defmodule Google.Apis.Base do
       end
 
       defp process_url(url) do
-        api_key = Application.fetch_env!(:google_api_client, :api_key)
-        "#{unquote(endpoint)}" <> url <> "&key=#{api_key}"
+        url = "#{unquote(endpoint)}#{url}"
+        case unquote(oauth_only) do
+          true -> url
+          _ ->
+            api_key = Application.fetch_env!(:google_api_client, :api_key)
+            uri = URI.parse(url)
+            query = URI.decode_query(uri.query || "")
+                    |> Map.merge(%{"key" => api_key})
+            uri
+            |> Map.put(:query, URI.encode_query(query))
+            |> URI.to_string
+        end
       end
     end
   end
