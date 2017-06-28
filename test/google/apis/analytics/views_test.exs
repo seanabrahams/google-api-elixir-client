b01ef082d1840492dce174a8bb401f7a4c63949f
elkelk <danielshaneupton@gmail.com>
2017-06-28 16:12:23 -0400

    Add Analytics.Views methods/tests

---
+++ test/google/apis/analytics/views_test.exs
@@ -0,0 +1,71 @@
+defmodule Google.Apis.Analytics.ViewsTest do
+  use Google.ApiCase
+  doctest Google.Apis.Analytics.Views
+  alias Google.Apis.Analytics.Views
+
+  @token "abc123"
+  @resource %{websiteUrl: "http://www.examplepetstore.com", name: "Example Store"}
+  @account_id "12345678"
+  @web_property_id "UA-12345678-6"
+  @profile_id "A1"
+
+  test "#get 401 response" do
+    use_cassette "analytics_views_get_error_response" do
+      assert {:error, %HTTPoison.Response{status_code: 401}} = Views.get(@token, @account_id, @web_property_id, @profile_id)
+    end
+  end
+
+  test "#get 200 response" do
+    use_cassette "analytics_views_get_response" do
+      assert {:ok, %{"id" => _id}} = Views.get(@token, @account_id, @web_property_id, @profile_id)
+    end
+  end
+
+  test "#insert 401 response" do
+    use_cassette "analytics_views_insert_error_response" do
+      assert {:error, %HTTPoison.Response{status_code: 401}} = Views.insert(@token, @account_id, @web_property_id, @resource)
+    end
+  end
+
+  test "#insert 200 response" do
+    use_cassette "analytics_views_insert_response" do
+      assert {:ok, %{"id" => _id}} = Views.insert(@token, @account_id, @web_property_id, @response)
+    end
+  end
+
+  test "#list 401 response" do
+    use_cassette "analytics_views_error_response" do
+      assert {:error, %HTTPoison.Response{status_code: 401}} = Views.list(@token, @account_id, @web_property_id)
+    end
+  end
+
+  test "#list 200 response" do
+    use_cassette "analytics_views_response" do
+      assert {:ok, %{"items" => _items}} = Views.list(@token, @account_id, @web_property_id)
+    end
+  end
+
+  test "#patch 401 response" do
+    use_cassette "analytics_views_patch_error_response" do
+      assert {:error, %HTTPoison.Response{status_code: 401}} = Views.patch(@token, @account_id, @web_property_id, @profile_id, @resource)
+    end
+  end
+
+  test "#patch 200 response" do
+    use_cassette "analytics_views_patch_response" do
+      assert {:ok, %{"id" => _id}} = Views.patch(@token, @account_id, @web_property_id, @profile_id, @response)
+    end
+  end
+
+  test "#update 401 response" do
+    use_cassette "analytics_views_update_error_response" do
+      assert {:error, %HTTPoison.Response{status_code: 401}} = Views.update(@token, @account_id, @web_property_id, @profile_id, @resource)
+    end
+  end
+
+  test "#update 200 response" do
+    use_cassette "analytics_views_update_response" do
+      assert {:ok, %{"id" => _id}} = Views.update(@token, @account_id, @web_property_id, @profile_id, @response)
+    end
+  end
+end
--- test/google/apis/analytics/web_properties_test.exs
+++ test/google/apis/analytics/web_properties_test.exs
@@ -5,64 +5,66 @@ defmodule Google.Apis.Analytics.WebPropertiesTest do
 
   @token "abc123"
   @resource %{websiteUrl: "http://www.examplepetstore.com", name: "Example Store"}
+  @account_id "12345678"
+  @web_property_id "UA-12345678-6"
 
   test "#get 401 response" do
     use_cassette "analytics_web_properties_get_error_response" do
-      assert {:error, %HTTPoison.Response{status_code: 401}} = WebProperties.get(@token, "12345678")
+      assert {:error, %HTTPoison.Response{status_code: 401}} = WebProperties.get(@token, @account_id, @web_property_id)
     end
   end
 
   test "#get 200 response" do
     use_cassette "analytics_web_properties_get_response" do
-      assert {:ok, %{"id" => _id}} = WebProperties.get(@token, "12345678", "UA-12345678-6")
+      assert {:ok, %{"id" => _id}} = WebProperties.get(@token, @account_id, @web_property_id)
     end
   end
 
   test "#insert 401 response" do
     use_cassette "analytics_web_properties_insert_error_response" do
-      assert {:error, %HTTPoison.Response{status_code: 401}} = WebProperties.insert(@token, "12345678", @resource)
+      assert {:error, %HTTPoison.Response{status_code: 401}} = WebProperties.insert(@token, @account_id, @resource)
     end
   end
 
   test "#insert 200 response" do
     use_cassette "analytics_web_properties_insert_response" do
-      assert {:ok, %{"id" => _id}} = WebProperties.insert(@token, "12345678", @response)
+      assert {:ok, %{"id" => _id}} = WebProperties.insert(@token, @account_id, @response)
     end
   end
 
   test "#list 401 response" do
     use_cassette "analytics_web_properties_error_response" do
-      assert {:error, %HTTPoison.Response{status_code: 401}} = WebProperties.list(@token, "12345678")
+      assert {:error, %HTTPoison.Response{status_code: 401}} = WebProperties.list(@token, @account_id)
     end
   end
 
   test "#list 200 response" do
     use_cassette "analytics_web_properties_response" do
-      assert {:ok, %{"items" => _items}} = WebProperties.list(@token, "12345678")
+      assert {:ok, %{"items" => _items}} = WebProperties.list(@token, @account_id)
     end
   end
 
   test "#patch 401 response" do
     use_cassette "analytics_web_properties_patch_error_response" do
-      assert {:error, %HTTPoison.Response{status_code: 401}} = WebProperties.patch(@token, "12345678", "UA-12345678-6", @resource)
+      assert {:error, %HTTPoison.Response{status_code: 401}} = WebProperties.patch(@token, @account_id, @web_property_id, @resource)
     end
   end
 
   test "#patch 200 response" do
     use_cassette "analytics_web_properties_patch_response" do
-      assert {:ok, %{"id" => _id}} = WebProperties.patch(@token, "12345678", "UA-12345678-6", @response)
+      assert {:ok, %{"id" => _id}} = WebProperties.patch(@token, @account_id, @web_property_id, @response)
     end
   end
 
   test "#update 401 response" do
     use_cassette "analytics_web_properties_update_error_response" do
-      assert {:error, %HTTPoison.Response{status_code: 401}} = WebProperties.update(@token, "12345678", "UA-12345678-6", @resource)
+      assert {:error, %HTTPoison.Response{status_code: 401}} = WebProperties.update(@token, @account_id, @web_property_id, @resource)
     end
   end
 
   test "#update 200 response" do
     use_cassette "analytics_web_properties_update_response" do
-      assert {:ok, %{"id" => _id}} = WebProperties.update(@token, "12345678", "UA-12345678-6", @response)
+      assert {:ok, %{"id" => _id}} = WebProperties.update(@token, @account_id, @web_property_id, @response)
     end
   end
 end
